c6a2f37400e457dbfbea6818db645a3ba86ff1a6
jacobotapia <jacob.et.cetera@gmail.com>
2017-02-28 23:05:18 -0600

    Hormiga

--- ACO/Hormiga.java
+++ ACO/Hormiga.java
@@ -1,3 +1,24 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Hormiga{
-		
+	
+	private List<Integer> visitados;
+	private List<Adyacencias> caminosUtilizados;
+	private int acumuladoCamino;
+
+	public Hormiga(){
+		this.visitados = new ArrayList<Integer>();
+		this.caminosUtilizados = new ArrayList<Adyacencias>();
+		this.acumuladoCamino = 0;
+	}
+
+	public int getAcumulado(){
+		return this.acumuladoCamino;
+	}
+
+	public void addAcumulado(int peso){
+		this.acumuladoCamino+=peso;
+	}
+
 }
